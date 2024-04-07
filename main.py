7b024db198a98de5722a0f952bbdcaf29c9e085a
Paul Stratton <paulstratton56@gmail.com>
2024-04-07 13:19:39 -0700

    front end scaffolding
    > Created a simple set of front end pages to utilize on the backend.

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():    
-    return render_template("index.html")
+    return redirect(url_for("login"))
+
+@app.route("/student/myClasses")
+def studentMyClasses():
+    return render_template("studentMyClasses.html")
+
+@app.route("/student/allClasses")
+def studentAllClasses():
+    return render_template("studentAllClasses.html")
+
+@app.route("/teacher/myClasses")
+def teacherMyClasses():
+    return render_template("teacherMyClasses.html")
+
+@app.route("/teacher/class/students")
+def teacherStudents():
+    return render_template("teacherStudents.html")
 
 @app.route("/login")
 def login():
